a2b08489a9b0917d33e0b4f67b3753bd64f0f37e
boelm <boel_mattsson@hotmail.com>
2024-07-03 11:07:48 +0200

    intial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Exercise 11");
+console.log("Exercise 10");
+console.log("Exercise 9");
+console.log("Exercise 8");
+console.log("Exercise 7");
+console.log("Exercise 6");
+console.log("Exercise 5");
+console.log("Exercise 4");
+console.log("Exercise 3");
+console.log("Exercise 2");
+console.log("Exercise 1");
+
+console.log("Exercise 0");
+console.log("Hej");
+let yourName = "Boel";
+console.log(`Hej ${yourName}!`);
+console.log(yourName);
